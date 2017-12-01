5d5e075d24ec95e15b53796abe3807020dcb844b
Jason Dulin <jas.dulin@gmail.com>
2017-12-01 08:49:59 -0700

    restaurant route refactoring

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,45 +1,73 @@
 const router = (module.exports = require("express").Router());
 var knex = require("../knex");
 const fetch = require('node-fetch')
+const restaurant = require('../lib/restaurant')
+const messaging = require('../lib/messaging')
+const port = parseInt(process.env.PORT || 3000)
 
-// Get all employees
+
 router.get("/", function(req, res) {
-  knex.select("id", "fName", "lName", "email")
-    .from("restaurant")
-    .then(data => {
-      res.json(data);
-    });
+  restaurant.getAll()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).send({error: err.message, stack: err.stack}))
 });
 
-// Get message by id
+// Get manager by id
 router.get('/:id', function (req, res) {
-  let managerId = req.params.id
-  knex.select("id", "fName", "lName", "email")
-    .from('restaurant')
-    .where('id', managerId)
-    .then( function (data) {
-      res.json(data)
-    })
+  restaurant.getOne(req.params)
+  .then(data => res.json(data))
+  .catch(err => res.status(500).send({error: err.message, stack: err.stack}))
 })
 
-// Create a new employee
+// Create a new restaurant
 router.post('/', function (req, res) {
-    var row = req.body
-    knex.select('email')
-        .from('restaurant')
-        .where('phone', row.email)
-        .then( (data) => {
-          res.json(data)
-        })
+restaurant.create(req.body)
+  .then(data => res.json(data))
+  .catch(err => res.status(500).send({error: err.message, stack: err.stack}))
 })
 
 // Delete a user
 router.delete('/:id', function (req, res) {
-  let managerId = req.params.id
-  knex('restaurant')
-    .where('restaurant.id', managerId)
-    .del()
-    .then( (data) => {
-      res.json(data)
-    })
+  restaurant.remove(req.params)
+  .then(data => res.json(data))
+  .catch(err => res.status(500).send({error: err.message, stack: err.stack}))
 })
+
+
+//OLD CODE
+// // call like: getLink(req)
+// function getLink({protocol, host, body}) {
+//   const {phone} = body
+//   return `${protocol}://${host}${port === 80 ? '' : ':' + port}/users/${phone}`
+// }
+
+// // Get all restaurants
+// router.get("/", function(req, res) {
+//   knex.select("id", "fName", "lName", "email")
+//     .from("restaurant")
+//     .then(data => {
+//       res.json(data);
+//     });
+// });
+//
+// // Get message by id
+// router.get('/:id', function (req, res) {
+//   let managerId = req.params.id
+//   knex.select("id", "fName", "lName", "email")
+//     .from('restaurant')
+//     .where('id', managerId)
+//     .then( function (data) {
+//       res.json(data)
+//     })
+// })
+
+// // Delete a user
+// router.delete('/:id', function (req, res) {
+//   let managerId = req.params.id
+//   knex('restaurant')
+//     .where('restaurant.id', managerId)
+//     .del()
+//     .then( (data) => {
+//       res.json(data)
+//     })
+// })
