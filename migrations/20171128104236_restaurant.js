fb9abf74848e51f55ce7bcf00d24be8d6c6d09b1
Jason Dulin <jas.dulin@gmail.com>
2017-12-06 10:36:39 -0700

    DB setup almost correct

--- migrations/20171128104236_restaurant.js
+++ migrations/20171128104236_restaurant.js
@@ -4,13 +4,13 @@ exports.up = function(knex, Promise) {
   table.text('fName');
   table.text('lName');
   table.text('email');
+  table.text('password');
   table.text('address');
   table.text('city');
   table.text('state');
   table.text('zip');
   table.text('lat');
   table.text('long');
-  table.text('password');
 });
 };
 
