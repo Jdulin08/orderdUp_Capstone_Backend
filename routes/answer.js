83944daa874d05d70a3f59555c664378952b8518
Jason Dulin <jas.dulin@gmail.com>
2017-12-06 10:47:18 -0700

    answer routes in progress

--- routes/answer.js
+++ routes/answer.js
@@ -1,44 +1,80 @@
 const router = (module.exports = require("express").Router());
-var knex = require("../knex");
+const answer = require('../lib/employee')
+const question = require('../lib/question')
+const port = parseInt(process.env.PORT || 3000)
 
-// Get all messages
+// Get all employees
 router.get("/", function(req, res) {
-  knex.select("id", "questionId", "parentId", "aText")
-    .from("answer")
-    .then(data => {
-      res.json(data);
-    });
+  // return res.send({ host: req.host, protocol: req.protocol, ip: req.ip })
+  answer.getAll()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).send({
+      error: err.message,
+      stack: err.stack
+    }))
 });
 
 // Get message by id
-router.get('/:id', function (req, res) {
-  let answerId = req.params.id
-  knex.select("id", "questionId", "parentId", "aText")
-    .from("answer")
-    .where('id', answerId)
-    .then( function (data) {
-      res.json(data)
-    })
+router.get('/:id', function(req, res) {
+  answer.getOne(req.params)
+    .then(data => res.json(data))
+    .catch(err => res.status(500).send({
+      error: err.message,
+      stack: err.stack
+    }))
 })
 
-// Create a custom message as a user
+// Create a new answer
 router.post('/', function (req, res) {
-  let answerRow = req.body
-  knex('answer')
-    .insert(answerRow)
-    .returning('*')
-    .then( (data) => {
-      res.json(data)
-    })
+answer.create(req.body)
+  .then(data => res.json(data))
+  .catch(err => res.status(500).send({error: err.message, stack: err.stack}))
 })
 
-// Delete a user
-router.delete('/:id', function (req, res) {
-  let answerId = req.params.id
-  knex('answer')
-    .where('answer.id', answerId)
-    .del()
-    .then( (data) => {
-      res.json(data)
-    })
-})
+
+//OLD CODE
+
+// const router = (module.exports = require("express").Router());
+// var knex = require("../knex");
+//
+// // Get all messages
+// router.get("/", function(req, res) {
+//   knex.select("id", "questionId", "parentId", "aText")
+//     .from("answer")
+//     .then(data => {
+//       res.json(data);
+//     });
+// });
+//
+// // Get message by id
+// router.get('/:id', function (req, res) {
+//   let answerId = req.params.id
+//   knex.select("id", "questionId", "parentId", "aText")
+//     .from("answer")
+//     .where('id', answerId)
+//     .then( function (data) {
+//       res.json(data)
+//     })
+// })
+//
+// // Create a custom message as a user
+// router.post('/', function (req, res) {
+//   let answerRow = req.body
+//   knex('answer')
+//     .insert(answerRow)
+//     .returning('*')
+//     .then( (data) => {
+//       res.json(data)
+//     })
+// })
+//
+// // Delete a user
+// router.delete('/:id', function (req, res) {
+//   let answerId = req.params.id
+//   knex('answer')
+//     .where('answer.id', answerId)
+//     .del()
+//     .then( (data) => {
+//       res.json(data)
+//     })
+// })
