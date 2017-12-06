6d108ed94df63347acb5434955a71e7a9f9903a4
Jason Dulin <jas.dulin@gmail.com>
2017-12-06 13:26:24 -0700

    changed answer table format for questions

--- seeds/04_answers.js
+++ seeds/04_answers.js
@@ -6,19 +6,34 @@ exports.seed = function(knex, Promise) {
       return knex('answer').insert([
         {questionId: "1",
         employeeId: "1",
-        managerId: "1",
+        managerId: "",
         aText: ""
         },
         {questionId: "2",
         employeeId: "2",
-        managerId: "2",
+        managerId: "",
         aText: ""
         },
         {questionId: "3",
         employeeId: "3",
-        managerId: "3",
+        managerId: "",
         aText: ""
         },
+        {questionId: "4",
+        employeeId: "",
+        managerId: "1",
+        aText: ""
+        },
+        {questionId: "5",
+        employeeId: "",
+        managerId: "2",
+        aText: ""
+        },
+        {questionId: "6",
+        employeeId: "",
+        managerId: "3",
+        aText: ""
+        }
       ]);
     });
 };
